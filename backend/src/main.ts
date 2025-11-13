d075bf56a3dea9cd692413221296b5ff3ae2dec6
Sanjay KUMAR <itechlicense@outlook.com>
2025-11-13 16:29:43 +0100

    Updated frontend and build settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import { Request, Response, NextFunction } from 'express';
+dotenv.config({ path: join(__dirname, '..', '.env') });
+
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Log every request path
+ app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
+
   app.useGlobalFilters(new AllExceptionsFilter());
+
+//app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Shift Log API')
@@ -16,9 +32,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+  origin: [
+    'https://ops-log.com',
+    'https://www.ops-log.com',
+    'http://localhost:3000' // keep for local dev
+  ],
+  credentials: true,
+});
+
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -30,5 +51,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 4000);
+  console.log(`🚀 Server running on port ${process.env.PORT ?? 4000}`);
 }
 void bootstrap();
